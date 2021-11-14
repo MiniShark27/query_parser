065afecca284343af06a70eff03a87707a1092b4
MiniShark27 <kobifalus@gmail.com>
2021-11-13 19:25:40 -0500

    Move database connection maker to own file

---
+++ src/main/java/app/GetConnection.java
@@ -0,0 +1,90 @@
+package app;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import javax.sql.DataSource;
+
+import org.apache.calcite.adapter.jdbc.JdbcSchema;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.schema.SchemaPlus;
+
+public class GetConnection {
+
+  public static SchemaPlus getSchemaFromProperties() throws SQLException {
+    Properties prop = new Properties();
+    try (InputStream input = new FileInputStream("config.properties")) {
+      prop.load(input);
+    } catch (IOException ex) {
+      System.out.println("Properties File Not Found, Making One");
+      try {
+        prop = getPostgresScemaProperties();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return getSchema(prop.getProperty("db.url"), prop.getProperty("db.user"), prop.getProperty("db.password"),
+          prop.getProperty("db.calciteDb"));
+  }
+
+  private static SchemaPlus getSchema(String dbUrl, String username, String password, String calciteDb)
+      throws SQLException {
+    System.out.println("Connecting to database at: " +dbUrl);
+    System.out.println("Username: "+username);
+    System.out.println("Password: "+password);
+    System.out.println("Will be stored in database: " +calciteDb);
+    DriverManager.registerDriver(new org.apache.calcite.jdbc.Driver());
+    Connection connection = DriverManager.getConnection("jdbc:calcite:");
+    CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
+
+    SchemaPlus rootSchema = calciteConnection.getRootSchema();
+
+    final DataSource ds = JdbcSchema.dataSource(dbUrl, "org.postgresql.Driver", username, password);
+    rootSchema.add(calciteDb, JdbcSchema.create(rootSchema, calciteDb, ds, null, null));
+    return rootSchema;
+  }
+
+  private static Properties getPostgresScemaProperties() throws IOException {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Pulling from local postgres database (hosted at 127.0.0.1 i.e. localhost)");
+
+    System.out.println("Please input the name of the maintenence database:");
+    String maintenenceDb = scanner.nextLine();
+    String calciteDb = maintenenceDb;
+
+    System.out.println("Please input the port where the database is:");
+    int port = scanner.nextInt();
+    //Clear Scanner buffer
+    scanner.nextLine();
+    String dbUrl = String.format("jdbc:postgresql://localhost:%d/%s", port, maintenenceDb);
+
+    System.out.println("Please input the username for the database:");
+    String username = scanner.nextLine();
+
+    System.out.println("Please input the password for the database:");
+    String password = scanner.nextLine();
+
+    OutputStream output = new FileOutputStream("config.properties");
+    Properties prop = new Properties();
+
+    prop.setProperty("db.url", dbUrl);
+    prop.setProperty("db.user", username);
+    prop.setProperty("db.password", password);
+    prop.setProperty("db.calciteDb", calciteDb);
+
+    prop.store(output, null);
+    System.out.println(prop);
+    scanner.close();
+    return prop;
+  }
+}
--- src/main/java/app/QueryParser.java
+++ src/main/java/app/QueryParser.java
@@ -3,15 +3,9 @@ package app;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import javax.sql.DataSource;
-
-import org.apache.calcite.adapter.jdbc.JdbcSchema;
-import org.apache.calcite.jdbc.CalciteConnection;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.RelWriter;
@@ -31,48 +25,6 @@ import org.apache.calcite.tools.ValidationException;
 public class QueryParser {
 
   static Scanner scanner = new Scanner(System.in);
-
-  public static SchemaPlus getPostgresScema() throws SQLException {
-
-    String username;
-    String password;
-    String dbUrl;
-    String calciteDb;
-
-    System.out.println("Pulling from local postgres database (hosted at 127.0.0.1 i.e. localhost)");
-    System.out.println("Please input the name of the maintenence database:");
-    String maintenenceDb = scanner.nextLine();
-    calciteDb = maintenenceDb;
-    System.out.println("Please input the port where the database is:");
-    int port = scanner.nextInt();
-    scanner.nextLine();
-    dbUrl = String.format("jdbc:postgresql://localhost:%d/%s", port, maintenenceDb);
-    System.out.println("Please input the username for the database:");
-    username = scanner.nextLine();
-    System.out.println("Please input the password for the database:");
-    password = scanner.nextLine();
-
-    System.out.println("Will connect to:");
-    System.out.println(dbUrl);
-    System.out.println("With username:");
-    System.out.println(username);
-
-    System.out.println("and password:");
-    System.out.println(password);
-    System.out.println("In calcite the db will be called:");
-    System.out.println(calciteDb);
-
-    DriverManager.registerDriver(new org.apache.calcite.jdbc.Driver());
-    Connection connection = DriverManager.getConnection("jdbc:calcite:");
-    CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
-
-    SchemaPlus rootSchema = calciteConnection.getRootSchema();
-
-    final DataSource ds = JdbcSchema.dataSource(dbUrl, "org.postgresql.Driver", username, password);
-    rootSchema.add(calciteDb, JdbcSchema.create(rootSchema, calciteDb, ds, null, null));
-
-    return rootSchema;
-  }
 
   public static String[] getQueriesFromFile(String fileName) {
     String input = "";
@@ -117,7 +69,7 @@ public class QueryParser {
   }
 
   public static void main(String... args) throws SQLException {
-    SchemaPlus rootSchema = getPostgresScema();
+    SchemaPlus rootSchema = GetConnection.getSchemaFromProperties();
     SqlParser.Config insensitiveParser = SqlParser.config().withCaseSensitive(false);
     FrameworkConfig config = Frameworks.newConfigBuilder().parserConfig(insensitiveParser).defaultSchema(rootSchema)
         .build();
